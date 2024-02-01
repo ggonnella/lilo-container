cb989c11d8ffb71c6abc78d8bac49c645a8d9bea
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2024-02-01 16:40:11 +0700

    change example run

--- run_example.sh
+++ run_example.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
 MEDAKA=r104_e81_sup_g5015
-./run_container.sh /srv/giorgio/virology/ASFV/reads/ $MEDAKA
+#RAW=/srv/giorgio/virology/ASFV/reads/20230908_03_R10/20230908_AFS_Run_03_R10/raw
+RAW=/srv/giorgio/virology/ASFV/reads/20230908_03_R10/tmp3
+./run_container.sh $RAW $MEDAKA
