2449fcda926a13d2ee17c83b52cebe3d4e1e334d
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2024-02-01 16:39:27 +0700

    run_lilo: active conda from it; set user/group of output

--- run_lilo.sh
+++ run_lilo.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo $MEDAKA
 echo "Running LILO Snakemake"
 if [ "$MEDAKA" == "" ]; then
   echo "The medaka variable was not set correctly." > /dev/stderr
@@ -8,8 +7,19 @@ fi
 echo "Medaka is $MEDAKA"
 NCORES=`nproc`
 echo "Using $NCORES cores..."
+
+source $HOME/miniforge/etc/profile.d/conda.sh
+source $HOME/miniforge/etc/profile.d/mamba.sh
+mamba activate LILO
+
+original_user=$(stat -c "%u" "workdir")
+original_group=$(stat -c "%g" "workdir")
+cd workdir
 snakemake -k \
-          -s /home/user/Lilo/LILO \
+          --use-conda \
+          -s $HOME/Lilo/LILO \
           --configfile $ASFVDIR/config.file \
+          --verbose \
           --config medaka=$MEDAKA \
           --cores $NCORES
+chown -R $original_user:$original_group .
